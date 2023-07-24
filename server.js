60c9e7562716885b89b4ddbbb3bc28ef37ffe209
mmarew@gmail.com <mmarew@gmail.com>
2023-07-24 18:33:06 +0300

    commiting

---
+++ server.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT;
+const userRouter = require("./server/api/users/user.router");
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/api/users", userRouter);
+// app.use("/api/users");
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
